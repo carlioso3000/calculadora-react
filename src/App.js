3a3458b259c858233a59d72879d79a1c0d14611e
carlioso3000 <carlioso3006@gmail.com>
2023-06-20 17:25:34 -0400

    recent changes

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ const calcularResultado = () => {
 
 
 
+
   return (
     <div className="App">
       <div className="freecodecamp-logo-contenedor">
@@ -49,40 +50,42 @@ const calcularResultado = () => {
 
 
         <div className="fila">
-          <Boton manejarClic={agregarInput}>1</Boton>
-          <Boton manejarClic={agregarInput}>2</Boton>
-          <Boton manejarClic={agregarInput}>3</Boton>
-          <Boton manejarClic={agregarInput}>+</Boton>
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
         </div>
 
         <div className="fila">
           <Boton manejarClic={agregarInput}>4</Boton>
           <Boton manejarClic={agregarInput}>5</Boton>
           <Boton manejarClic={agregarInput}>6</Boton>
-          <Boton manejarClic={agregarInput}>-</Boton>
-
-        </div>
-
-        <div className="fila">
-          <Boton manejarClic={agregarInput}>7</Boton>
-          <Boton manejarClic={agregarInput}>8</Boton>
-          <Boton manejarClic={agregarInput}>9</Boton>
           <Boton manejarClic={agregarInput}>*</Boton>
 
         </div>
 
         <div className="fila">
-          <Boton manejarClic={calcularResultado}>=</Boton>
-          <Boton manejarClic={agregarInput}>0</Boton>
-          <Boton manejarClic={agregarInput}>.</Boton>
-          <Boton manejarClic={agregarInput}>/</Boton>
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
 
         </div>
 
         <div className="fila">
-          <BotonClear manejarClear={() => setInput("")}>
-            Clear
+          
+        <BotonClear manejarClear={() => setInput("")}>
+            C
           </BotonClear>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+
+        </div>
+
+        <div className="fila">
+          
+        <Boton manejarClic={calcularResultado}>=</Boton>
 
         </div>
 
